969b68f6161efe18c081b59cf3bd1d25bb99ef0e
MelisaTaskin <melisatskknn@gmail.com>
2022-09-05 01:03:59 +0300

    04.09.2022

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static  class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "ürün ismi geçersiz ";
+        internal static string MaintenanceTime = "sistem bakımda";
+        internal static string ProductListed = "ürüner listelendi";
+    }
+}
